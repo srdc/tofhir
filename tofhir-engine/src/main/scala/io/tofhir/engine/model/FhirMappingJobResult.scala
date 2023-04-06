2b452e94d5dad82625d4cf9cac3b42abd9f8fa93
A. Anil Sinaci <anil@srdc.com.tr>
2023-04-06 15:20:16 +0300

    :wrench: chore(logback): Simplify logback configuration.

--- tofhir-engine/src/main/scala/io/tofhir/engine/model/FhirMappingJobResult.scala
+++ tofhir-engine/src/main/scala/io/tofhir/engine/model/FhirMappingJobResult.scala
@@ -4,23 +4,24 @@ import net.logstash.logback.marker.LogstashMarker
 import net.logstash.logback.marker.Markers.{append}
 
 /**
- * Result of a mapping job for a batch of input
- * @param mappingJobExecution    Fhir Mapping Job execution
- * @param mappingUrl             Url of the mapping if this is reporting single mapping
- * @param numOfInvalids          Total number of invalid input rows
- * @param numOfNotMapped         Total number of mappings per row that is not successful
- * @param numOfFhirResources     Total number of FHIR resources created as a result mapping(s)
- * @param numOfFailedWrites      Total number of FHIR resources that cannot be written to the configured sink (e.g. FHIR repository)
+ * Result of a batch mapping job execution
+ *
+ * @param mappingJobExecution Fhir Mapping Job execution
+ * @param mappingUrl          Url of the mapping if this is reporting single mapping
+ * @param numOfInvalids       Total number of invalid input rows
+ * @param numOfNotMapped      Total number of mappings per row that is not successful
+ * @param numOfFhirResources  Total number of FHIR resources created as a result mapping(s)
+ * @param numOfFailedWrites   Total number of FHIR resources that cannot be written to the configured sink (e.g. FHIR repository)
  */
 case class FhirMappingJobResult(mappingJobExecution: FhirMappingJobExecution,
-                                mappingUrl:Option[String],
-                                numOfInvalids:Long = -1,
-                                numOfNotMapped:Long = -1,
-                                numOfFhirResources:Long = -1,
-                                numOfFailedWrites:Long = -1
+                                mappingUrl: Option[String],
+                                numOfInvalids: Long = -1,
+                                numOfNotMapped: Long = -1,
+                                numOfFhirResources: Long = -1,
+                                numOfFailedWrites: Long = -1
                                ) {
-  final val eventId:String = "MAPPING_JOB_RESULT"
-  val  result:String = (numOfInvalids + numOfNotMapped + numOfFailedWrites) match {
+  final val eventId: String = "MAPPING_JOB_RESULT"
+  val result: String = (numOfInvalids + numOfNotMapped + numOfFailedWrites) match {
     case 0 => "SUCCESS"
     case neg if neg < 0 => "FAILURE"
     case _ => "PARTIAL_SUCCESS"
@@ -28,11 +29,11 @@ case class FhirMappingJobResult(mappingJobExecution: FhirMappingJobExecution,
 
   override def toString: String = {
     s"toFHIR batch mapping result ($result) for execution '${mappingJobExecution.id}' of job '${mappingJobExecution.jobId}' in project '${mappingJobExecution.projectId}'${mappingUrl.map(u => s" for mapping '$u'").getOrElse("")}!\n" +
-      (if(result != "FAILURE")
+      (if (result != "FAILURE")
         s"\t# of Invalid Rows: \t$numOfInvalids\n" +
-        s"\t# of Not Mapped: \t$numOfNotMapped\n" +
-        s"\t# of FHIR resources:\t$numOfFhirResources\n" +
-        s"\t# of Failed writes:\t$numOfFailedWrites"
+          s"\t# of Not Mapped: \t$numOfNotMapped\n" +
+          s"\t# of FHIR resources:\t$numOfFhirResources\n" +
+          s"\t# of Failed writes:\t$numOfFailedWrites"
       else
         ""
         )
@@ -44,14 +45,14 @@ case class FhirMappingJobResult(mappingJobExecution: FhirMappingJobExecution,
    */
   def toLogstashMarker: LogstashMarker = {
     append("jobId", mappingJobExecution.jobId)
-      .and(append("projectId",mappingJobExecution.projectId)
-      .and(append("executionId",mappingJobExecution.id)
-      .and(append("mappingUrl", mappingUrl.orElse(null))
-        .and(append("result", result)
-          .and(append("numOfInvalids", numOfInvalids)
-          .and(append("numOfNotMapped", numOfNotMapped)
-          .and(append("numOfFhirResources", numOfFhirResources)
-            .and(append("numOfFailedWrites", numOfFailedWrites)
-              .and(append("eventId", eventId))))))))))
+      .and(append("projectId", mappingJobExecution.projectId)
+        .and(append("executionId", mappingJobExecution.id)
+          .and(append("mappingUrl", mappingUrl.orElse(null))
+            .and(append("result", result)
+              .and(append("numOfInvalids", numOfInvalids)
+                .and(append("numOfNotMapped", numOfNotMapped)
+                  .and(append("numOfFhirResources", numOfFhirResources)
+                    .and(append("numOfFailedWrites", numOfFailedWrites)
+                      .and(append("eventId", eventId))))))))))
   }
 }
