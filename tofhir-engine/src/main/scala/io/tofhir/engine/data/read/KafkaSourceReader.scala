eb37e1d1cb2118ad0b55178eafec59e142cc618f
A. Anil Sinaci <anil@srdc.com.tr>
2023-04-13 17:47:43 +0300

    :recycle: refactor(engine): Refactor KafkaSourceReader's type processing within JSON values.

--- tofhir-engine/src/main/scala/io/tofhir/engine/data/read/KafkaSourceReader.scala
+++ tofhir-engine/src/main/scala/io/tofhir/engine/data/read/KafkaSourceReader.scala
@@ -1,17 +1,16 @@
 package io.tofhir.engine.data.read
 
-import io.tofhir.engine.model.{KafkaSource, KafkaSourceSettings}
-import org.apache.spark.sql.functions.{col, from_json, udf}
-import org.apache.spark.sql.types.{BooleanType, DoubleType, IntegerType, LongType, StringType, StructType}
-import org.apache.spark.sql.{DataFrame, SparkSession}
-import java.time.LocalDateTime
-
 import com.fasterxml.jackson.core.JsonParseException
 import io.onfhir.api.Resource
 import io.onfhir.util.JsonFormatter._
+import io.tofhir.engine.model.{KafkaSource, KafkaSourceSettings}
+import org.apache.spark.sql.functions.{col, from_json, udf}
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.json4s.{JBool, JLong, JNull, JDouble, JInt}
+
+import java.time.LocalDateTime
 import javax.ws.rs.InternalServerErrorException
-import org.json4s.{JBool, JLong, JNull}
-import org.json4s.JsonAST.{JDouble, JInt}
 
 /**
  *
@@ -29,56 +28,50 @@ class KafkaSourceReader(spark: SparkSession) extends BaseDataSourceReader[KafkaS
   override def read(mappingSource: KafkaSource, sourceSettings: KafkaSourceSettings, schema: Option[StructType] = Option.empty, timeRange: Option[(LocalDateTime, LocalDateTime)] = Option.empty): DataFrame = {
     import spark.implicits._
 
-    if(schema.isEmpty) {
+    if (schema.isEmpty) {
       throw new IllegalArgumentException("Schema is required for streaming source")
     }
 
     // user-defined function (UDF) to process message (json) coming from the Kafka topic
-    val processDataUDF = udf((message:String) => {
-      var json:Resource = null
-      // try-catch block needed to handle unparseable json
-      try {
-        json = message.parseJson
-      }
-      catch {
-        case e:JsonParseException =>  throw new InternalServerErrorException("Kafka message is an unparseable json",e)
-      }
+    val processDataUDF = udf((message: String) => {
+      val json: Resource =
+        try { // try-catch block needed to handle unparseable json
+          message.parseJson
+        } catch {
+          case e: JsonParseException => throw new InternalServerErrorException("Kafka message is an unparseable JSON", e)
+        }
+
       // process each field so that string values are acceptable (if they are parseable of course) for some data types such as double and integer
       json.mapField(field => {
-        // get field type from the schema
-        val fieldType = schema.get.fields.find(p => p.name.contentEquals(field._1)).orNull
-        if(fieldType == null){
-          // the data contains a field which does not exist in the schema, no need to process it
-          field
-        } else {
+        schema.get.fields.find(p => p.name.contentEquals(field._1)).map(fieldType => { // get field type from the schema
           fieldType.dataType match {
-            case _:DoubleType =>
+            case _: DoubleType =>
               try {
-                (field._1, if(field._2.extract[String].isEmpty) JNull else JDouble(field._2.extract[String].toDouble))
+                field._1 -> field._2.extractOpt[JDouble].getOrElse(JNull)
               } catch {
-                case e: NumberFormatException => throw new InternalServerErrorException(s"${field._2} is not a parsable `Double`",e)
+                case e: NumberFormatException => throw new InternalServerErrorException(s"${field._2} is not a parsable `Double`", e)
               }
-            case _:IntegerType =>
+            case _: IntegerType =>
               try {
-                (field._1, if(field._2.extract[String].isEmpty) JNull else JInt(field._2.extract[String].toInt))
+                field._1 -> field._2.extractOpt[JInt].getOrElse(JNull)
               } catch {
-                case e: NumberFormatException => throw new InternalServerErrorException(s"${field._2} is not a parsable `Integer`",e)
+                case e: NumberFormatException => throw new InternalServerErrorException(s"${field._2} is not a parsable `Integer`", e)
               }
-            case _:LongType =>
+            case _: LongType =>
               try {
-                (field._1, if(field._2.extract[String].isEmpty) JNull else JLong(field._2.extract[String].toLong))
+                field._1 -> field._2.extractOpt[JLong].getOrElse(JNull)
               } catch {
-                case e: NumberFormatException => throw new InternalServerErrorException(s"${field._2} is not a parsable `Long`",e)
+                case e: NumberFormatException => throw new InternalServerErrorException(s"${field._2} is not a parsable `Long`", e)
               }
-            case _:BooleanType =>
+            case _: BooleanType =>
               try {
-                (field._1, if(field._2.extract[String].isEmpty) JNull else JBool(field._2.extract[String].toBoolean))
+                field._1 -> field._2.extractOpt[JBool].getOrElse(JNull)
               } catch {
-                case e: IllegalArgumentException => throw new InternalServerErrorException(s"${field._2} is not a parsable `Boolean`",e)
+                case e: IllegalArgumentException => throw new InternalServerErrorException(s"${field._2} is not a parsable `Boolean`", e)
               }
             case _ => field
           }
-        }
+        }).getOrElse(field)
       }).toJson
     })
 
@@ -88,10 +81,10 @@ class KafkaSourceReader(spark: SparkSession) extends BaseDataSourceReader[KafkaS
       .option("kafka.bootstrap.servers", sourceSettings.bootstrapServers)
       .option("subscribe", mappingSource.topicName)
       .option("startingOffsets", mappingSource.startingOffsets)
-      .option("inferSchema", true)
+      .option("inferSchema", value = true)
       .load()
       .select($"value".cast(StringType)) // change the type of message from binary to string
-      .withColumn("value",processDataUDF(col("value"))) // replace 'value' column with the processed data
+      .withColumn("value", processDataUDF(col("value"))) // replace 'value' column with the processed data
       .select(from_json($"value", schema.get).as("record"))
       .select("record.*")
     df
