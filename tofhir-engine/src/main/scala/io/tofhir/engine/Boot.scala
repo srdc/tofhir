853926140ea22233fdaa16b8ea728871154f4a4b
Suat Gonul <suat@srdc.com.tr>
2023-05-18 16:44:41 +0300

    :ok_hand: (Engine): Wrapped engine bootstrap logic into a method to be able to call it from other environments

--- tofhir-engine/src/main/scala/io/tofhir/engine/Boot.scala
+++ tofhir-engine/src/main/scala/io/tofhir/engine/Boot.scala
@@ -1,5 +1,6 @@
 package io.tofhir.engine
 
+import io.onfhir.path.IFhirPathFunctionLibraryFactory
 import io.tofhir.engine.cli.{CommandExecutionContext, CommandFactory, CommandLineInterface}
 import io.tofhir.engine.config.ToFhirConfig
 
@@ -7,37 +8,39 @@ import io.tofhir.engine.config.ToFhirConfig
  * Entrypoint of toFHIR
  */
 object Boot extends App {
+  init()
 
-  val options = CommandLineInterface.nextArg(Map(), args.toList)
-  //Interactive command line interface
-  if (options.isEmpty || !options.contains("command") || options("command").asInstanceOf[String] == "cli") {
-    val toFhirEngine = new ToFhirEngine()
-    CommandLineInterface.start(toFhirEngine, ToFhirConfig.engineConfig.initialMappingJobFilePath)
+  def init(functionLibraryFactories : Map[String, IFhirPathFunctionLibraryFactory] = Map.empty): Unit = {
+    val options = CommandLineInterface.nextArg(Map(), args.toList)
+    //Interactive command line interface
+    if (options.isEmpty || !options.contains("command") || options("command").asInstanceOf[String] == "cli") {
+      val toFhirEngine = new ToFhirEngine(functionLibraryFactories = functionLibraryFactories)
+      CommandLineInterface.start(toFhirEngine, ToFhirConfig.engineConfig.initialMappingJobFilePath)
+    }
+    // Extract schemas from a REDCap data dictionary
+    else if (options("command").asInstanceOf[String] == "extract-redcap-schemas") {
+      val toFhirEngine = new ToFhirEngine(functionLibraryFactories = functionLibraryFactories)
+      // get parameters
+      val dataDictionary = options.get("data-dictionary").map(_.asInstanceOf[String])
+      val definitionRootUrl = options.get("definition-root-url").map(_.asInstanceOf[String])
+      val commandArgs: Seq[String] = Seq(dataDictionary, definitionRootUrl).filter(arg => arg.nonEmpty).map(arg => arg.get)
+      // run command
+      CommandFactory.apply("extract-redcap-schemas").execute(commandArgs, CommandExecutionContext(toFhirEngine))
+    }
+    //Run as batch job
+    else if (options("command").asInstanceOf[String] == "run") {
+      val toFhirEngine = new ToFhirEngine(functionLibraryFactories = functionLibraryFactories)
+      val mappingJobFilePath =
+        if (options.contains("job"))
+          options.get("job").map(_.asInstanceOf[String])
+        else
+          ToFhirConfig.engineConfig.initialMappingJobFilePath
+
+      val toFhirDbFolderPath =
+        if (options.contains("db-path")) options("db-path").asInstanceOf[String]
+        else ToFhirConfig.engineConfig.toFhirDbFolderPath
+
+      CommandLineInterface.runJob(toFhirEngine, mappingJobFilePath, toFhirDbFolderPath)
+    }
   }
-  // Extract schemas from a REDCap data dictionary
-  else if(options("command").asInstanceOf[String] == "extract-redcap-schemas") {
-    val toFhirEngine = new ToFhirEngine()
-    // get parameters
-    val dataDictionary = options.get("data-dictionary").map(_.asInstanceOf[String])
-    val definitionRootUrl = options.get("definition-root-url").map(_.asInstanceOf[String])
-    val commandArgs:Seq[String] = Seq(dataDictionary, definitionRootUrl).filter(arg => arg.nonEmpty).map(arg => arg.get)
-    // run command
-    CommandFactory.apply("extract-redcap-schemas").execute(commandArgs, CommandExecutionContext(toFhirEngine))
-  }
-  //Run as batch job
-  else if (options("command").asInstanceOf[String] == "run") {
-    val toFhirEngine = new ToFhirEngine()
-    val mappingJobFilePath =
-      if (options.contains("job"))
-        options.get("job").map(_.asInstanceOf[String])
-      else
-        ToFhirConfig.engineConfig.initialMappingJobFilePath
-
-    val toFhirDbFolderPath =
-      if (options.contains("db-path")) options("db-path").asInstanceOf[String]
-      else ToFhirConfig.engineConfig.toFhirDbFolderPath
-
-    CommandLineInterface.runJob(toFhirEngine, mappingJobFilePath, toFhirDbFolderPath)
-  }
-
 }
