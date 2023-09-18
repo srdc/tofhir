9323a62f5391ea05a8d292faac3b2d81bc1c33e5
Yunus Emre Gurses <gursesyunus@hotmail.com>
2023-09-18 11:07:12 +0300

    :sparkles: feat(SparkArchiver): add spark archiver to archive data sources row by row based on error types (e.g. invalid input, mapping error, invalid resource) to configured folder

--- tofhir-engine/src/main/scala/io/tofhir/engine/model/FhirMappingJobExecution.scala
+++ tofhir-engine/src/main/scala/io/tofhir/engine/model/FhirMappingJobExecution.scala
@@ -1,6 +1,6 @@
 package io.tofhir.engine.model
 
-import io.tofhir.engine.config.ErrorHandlingType
+import io.tofhir.engine.config.{ErrorHandlingType, ToFhirConfig}
 import io.tofhir.engine.config.ErrorHandlingType.ErrorHandlingType
 
 import java.util.UUID
@@ -28,4 +28,17 @@ case class FhirMappingJobExecution(id: String = UUID.randomUUID().toString,
    */
   def getCheckpointDirectory(mappingUrl: String): String =
     s"./checkpoint/$jobId/${mappingUrl.hashCode}"
+
+  /**
+   * Creates a error output directory for a mapping execution included in a job and an execution
+   * error-folder-path\<error-type>\job-<jobId>\execution-<executionId>\<hashedMappingUrl>\<random-generated-name-by-spark>.csv
+   * e.g. error-folder-path\invalid_input\job-d13b5044-f05c-4698-86c2-d83b3c5083f8\execution-59733de5-1c92-4741-b032-6e9e13ee4550\-521848504\part-00000-1d7d9467-0195-4d28-964d-89171727fa41-c000.csv
+   *
+   * @param mappingUrl
+   * @param errorType
+   * @return
+   */
+  def getErrorOutputDirectory(mappingUrl: String, errorType: String): String =
+    s"${ToFhirConfig.engineConfig.errorFolderPath}/${errorType}/job-$jobId/execution-${id}/${mappingUrl.hashCode}"
+
 }
