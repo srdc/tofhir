b0af12616b8e8f4baf7f1f46268ef555379d0a28
Yunus Emre Gurses <gursesyunus@hotmail.com>
2023-09-21 17:41:53 +0300

    :recycle: refactor(FhirMappingJobExecution): use alphanumeric version of mapping task name when creating a folder name with it in the erroneous records folder

--- tofhir-engine/src/main/scala/io/tofhir/engine/model/FhirMappingJobExecution.scala
+++ tofhir-engine/src/main/scala/io/tofhir/engine/model/FhirMappingJobExecution.scala
@@ -4,6 +4,7 @@ import io.tofhir.engine.config.{ErrorHandlingType, ToFhirConfig}
 import io.tofhir.engine.config.ErrorHandlingType.ErrorHandlingType
 
 import java.util.UUID
+import java.util.regex.Pattern
 
 /**
  * Represents the execution of mapping tasks included in a mapping job.
@@ -39,6 +40,29 @@ case class FhirMappingJobExecution(id: String = UUID.randomUUID().toString,
    * @return
    */
   def getErrorOutputDirectory(mappingUrl: String, errorType: String): String =
-    s"${ToFhirConfig.engineConfig.erroneousRecordsFolder}/${errorType}/job-${job.id}/execution-${id}/${mappingUrl.hashCode}"
+    s"${ToFhirConfig.engineConfig.erroneousRecordsFolder}/${errorType}/job-${job.id}/execution-${id}/${this.convertUrlToAlphaNumeric(mappingUrl)}"
+
+
+  /**
+   * Convert mapping url to alphanumeric string to be used as folder name
+   * e.g. https://aiccelerate.eu/fhir/mappings/streaming-test/patient-mapping -> https_aiccelerate_eu_fhir_mappings_streaming_test_patient_mapping
+   * @param url
+   * @return
+   */
+  private def convertUrlToAlphaNumeric(url: String): String = {
+    // Define a regular expression pattern to match alphanumeric parts
+    val pattern = Pattern.compile("[a-zA-Z0-9]+")
+    val matcher = pattern.matcher(url)
+    // Extract meaningful words and transform them
+    val extractedWords = scala.collection.mutable.ListBuffer[String]()
+    while (matcher.find()) {
+      val word = matcher.group()
+      // You can further transform the word if needed (e.g., removing spaces or special characters)
+      // For simplicity, we are not doing any additional transformation here.
+      extractedWords += word
+    }
+    // Combine the transformed words to create a folder name
+    extractedWords.mkString("-")
+  }
 
 }
