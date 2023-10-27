217b36b8da25bd6e19fbf2daa8ccc3347f4ca7ee
okanmercan <okanmercan16@gmail.com>
2023-10-27 16:15:15 +0300

    :sparkles: test(FileStreamInputArchiver): Add deletion test for batch and streaming jobs.

--- tofhir-engine/src/test/scala/io/tofhir/test/engine/execution/FileStreamInputArchiverTest.scala
+++ tofhir-engine/src/test/scala/io/tofhir/test/engine/execution/FileStreamInputArchiverTest.scala
@@ -80,6 +80,60 @@ class FileStreamInputArchiverTest extends AnyFlatSpec with Matchers {
     org.apache.commons.io.FileUtils.deleteDirectory(FileUtils.getPath(ToFhirConfig.engineConfig.archiveFolder).toFile)
   }
 
+  "FileStreamInputArchiver" should "apply deletion for a streaming job" in {
+
+    // Create a mock execution
+    val mockTaskExecution = mock[FhirMappingJobExecution]
+    val mockJob = mock[FhirMappingJob]
+    val mockDataProcessingSettings = mock[DataProcessingSettings]
+    val mappingUrl = "mocked_mapping_url"
+    val jobId = "mocked_job_id_2"
+    when(mockTaskExecution.id).thenReturn(jobId)
+    when(mockTaskExecution.job).thenReturn(mockJob)
+    when(mockJob.dataProcessingSettings).thenReturn(mockDataProcessingSettings)
+    when(mockDataProcessingSettings.archiveMode).thenReturn(ArchiveModes.DELETE)
+    when(mockTaskExecution.getCheckpointDirectory(mappingUrl)).thenReturn(
+      FileUtils.getPath("test-archiver", jobId, mappingUrl.hashCode.toString).toString)
+
+
+    // Create a source file to refer location of test.csv
+    val sourceFile = FileUtils.getPath("test-archiver", jobId, mappingUrl.hashCode.toString, "sources", "0", "0").toFile
+    // Path of test.csv
+    val testCsvFile = FileUtils.getPath("test-archiver", "test.csv").toFile
+    // Ensure the parent directories exist, if not, create them
+    sourceFile.getParentFile.mkdirs()
+    testCsvFile.getParentFile.mkdirs()
+    // Write path of test.csv to the source file
+    val sourceWriter = new PrintWriter(sourceFile)
+    sourceWriter.write(s"{\"path\":\"${testCsvFile.getAbsolutePath.replace("\\", "\\\\")}\"}")
+    sourceWriter.close()
+
+    // Create a test csv
+    val testCsvWriter = new PrintWriter(testCsvFile)
+    testCsvWriter.write("testColumn1,testColumn2,testColumn3,testColumn4,testColumn5\ntestRow1,testRow2,testRow3,testRow4,testRow5")
+    testCsvWriter.close()
+
+    // Create a commit file inorder to start range function in applyArchivingOnStreamingJob
+    val commitFile = FileUtils.getPath("test-archiver", jobId, mappingUrl.hashCode.toString, "commits", "0").toFile
+    // Ensure the parent directories exist, if not, create them
+    commitFile.getParentFile.mkdirs()
+    val commitWriter = new PrintWriter(commitFile)
+    commitWriter.write("v1\n{\"nextBatchWatermarkMs\":0}")
+    commitWriter.close()
+
+    // Check whether csv file exists
+    testCsvFile.exists() shouldBe true
+
+    // Call archiving function
+    fileStreamInputArchiver.applyArchivingOnStreamingJob(mockTaskExecution, mappingUrl)
+
+    // Check whether csv file is deleted
+    testCsvFile.exists() shouldBe false
+
+    // Clean test directory
+    org.apache.commons.io.FileUtils.deleteDirectory(FileUtils.getPath("test-archiver").toFile)
+  }
+
   "FileStreamInputArchiver" should "apply archiving for a batch job" in{
     // Create a mock execution
     val mockTaskExecution = mock[FhirMappingJobExecution]
@@ -131,5 +185,47 @@ class FileStreamInputArchiverTest extends AnyFlatSpec with Matchers {
     // Clean test directories
     org.apache.commons.io.FileUtils.deleteDirectory(FileUtils.getPath(ToFhirConfig.engineConfig.contextPath, sourceFolderPath).toFile)
     org.apache.commons.io.FileUtils.deleteDirectory(FileUtils.getPath(ToFhirConfig.engineConfig.archiveFolder).toFile)
+  }
+
+  "FileStreamInputArchiver" should "apply deletion for a batch job" in{
+    // Create a mock execution
+    val mockTaskExecution = mock[FhirMappingJobExecution]
+    val mockJob = mock[FhirMappingJob]
+    val mockDataProcessingSettings = mock[DataProcessingSettings]
+    val mockFileSystemSourceSettings = mock[FileSystemSourceSettings]
+    val mockFileSystemSource = mock[FileSystemSource]
+    val mockMappingTask = mock[FhirMappingTask]
+    val sourceFolderPath = "test-archiver-batch"
+    val inputFilePath = "test-input-file"
+    val jobId = "mocked_job_id_2"
+    when(mockTaskExecution.id).thenReturn(jobId)
+    when(mockTaskExecution.job).thenReturn(mockJob)
+    when(mockTaskExecution.mappingTasks).thenReturn(Seq(mockMappingTask))
+    when(mockJob.dataProcessingSettings).thenReturn(mockDataProcessingSettings)
+    when(mockDataProcessingSettings.archiveMode).thenReturn(ArchiveModes.DELETE)
+    when(mockJob.sourceSettings).thenReturn(Map(("_"->mockFileSystemSourceSettings)))
+    when(mockFileSystemSourceSettings.dataFolderPath).thenReturn(sourceFolderPath)
+    when(mockMappingTask.sourceContext).thenReturn(Map(("_" -> mockFileSystemSource)))
+    when(mockFileSystemSource.path).thenReturn(inputFilePath)
+
+    // Create a test input file
+    val inputFile = FileUtils.getPath(ToFhirConfig.engineConfig.contextPath, sourceFolderPath, inputFilePath).toFile
+    inputFile.getParentFile.mkdirs()
+    // Write test to input file
+    val inputWriter = new PrintWriter(inputFile)
+    inputWriter.write("test")
+    inputWriter.close()
+
+    // Check whether input file exists
+    inputFile.exists() shouldBe true
+
+    //Call archiving function
+    FileStreamInputArchiver.applyArchivingOnBatchJob(mockTaskExecution)
+
+    // Check whether input file is deleted
+    inputFile.exists() shouldBe false
+
+    // Clean test directories
+    org.apache.commons.io.FileUtils.deleteDirectory(FileUtils.getPath(ToFhirConfig.engineConfig.contextPath, sourceFolderPath).toFile)
   }
 }
