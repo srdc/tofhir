492081de821f0605d68e93e0b27bc0f4f33c3fb7
A. Anil Sinaci <anil@srdc.com.tr>
2023-11-29 22:11:24 +0300

    :sparkles: feat: Start versioning toFHIR from 1.0.0. The Maven version will be logged at the very beginning of the application as app version.

--- docker/engine/build.sh
+++ docker/engine/build.sh
@@ -1,4 +1,5 @@
 # Execute one of the following commands from the project.root.directory (../../)
+# Do not forget to use the correct version while tagging.
 
-docker build -f docker/engine/Dockerfile-addJar -t srdc/tofhir:latest -t srdc/tofhir:1.0 .
-docker build -f docker/engine/Dockerfile-buildJar -t srdc/tofhir:latest -t srdc/tofhir:1.0 .
+docker build -f docker/engine/Dockerfile-addJar -t srdc/tofhir:latest -t srdc/tofhir:1.0.0 .
+docker build -f docker/engine/Dockerfile-buildJar -t srdc/tofhir:latest -t srdc/tofhir:1.0.0 .
