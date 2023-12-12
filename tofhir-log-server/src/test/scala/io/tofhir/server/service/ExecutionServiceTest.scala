32feaf150bf5e5b67d9cac08d6743216f145ee8c
Emre Çam <cam.emre090@gmail.com>
2023-12-12 16:49:34 +0300

    ✅test: decrease logs in sample-logs file and update the ExecutionServiceTest

--- tofhir-log-server/src/test/scala/io/tofhir/server/service/ExecutionServiceTest.scala
+++ tofhir-log-server/src/test/scala/io/tofhir/server/service/ExecutionServiceTest.scala
@@ -24,13 +24,13 @@ class ExecutionServiceTest extends AsyncWordSpec with Matchers with BeforeAndAft
           executionsData.length shouldEqual count
           (executionsData.head \ "id").extract[String] shouldEqual "1f47ee28-65b8-48b5-bbd6-347b15254cfb"
           (executionsData.head \ "errorStatus").extract[String] shouldEqual "SUCCESS"
-          (executionsData.head \ "mappingUrls").extract[Seq[String]].length shouldEqual 21
+          (executionsData.head \ "mappingUrls").extract[Seq[String]].length shouldEqual 4
 
           (executionsData(5) \ "id").extract[String] shouldEqual "4df92d34-7712-47a7-8677-15339797c554"
           (executionsData(5) \ "errorStatus").extract[String] shouldEqual "PARTIAL_SUCCESS"
-          (executionsData(5) \ "mappingUrls").extract[Seq[String]].length shouldEqual 21
+          (executionsData(5) \ "mappingUrls").extract[Seq[String]].length shouldEqual 6
 
-          (executionsData(5) \ "mappingUrls")(1).extract[String] shouldEqual "https://aiccelerate.eu/fhir/mappings/pilot1/practitioner-mapping"
+          (executionsData(5) \ "mappingUrls")(1).extract[String] shouldEqual "https://aiccelerate.eu/fhir/mappings/pilot1/health-behavior-assessment-mapping"
         })
     }
 
@@ -46,8 +46,8 @@ class ExecutionServiceTest extends AsyncWordSpec with Matchers with BeforeAndAft
           executionsData.length shouldEqual filteredCount
           (executionsData.head \ "id").extract[String] shouldEqual "1f47ee28-65b8-48b5-bbd6-347b15254cfb"
           (executionsData.head \ "errorStatus").extract[String] shouldEqual "SUCCESS"
-          (executionsData.head \ "mappingUrls").extract[Seq[String]].length shouldEqual 21
-          (executionsData.head \ "mappingUrls")(1).extract[String] shouldEqual "https://aiccelerate.eu/fhir/mappings/pilot1/practitioner-mapping"
+          (executionsData.head \ "mappingUrls").extract[Seq[String]].length shouldEqual 4
+          (executionsData.head \ "mappingUrls")(1).extract[String] shouldEqual "https://aiccelerate.eu/fhir/mappings/pilot1/vital-signs-mapping"
         })
     }
 
@@ -76,9 +76,9 @@ class ExecutionServiceTest extends AsyncWordSpec with Matchers with BeforeAndAft
           executionsData.length shouldEqual filteredCount
           (executionsData(1) \ "id").extract[String] shouldEqual "57bb4431-5d58-44d5-b525-a838c0cfa21c"
           (executionsData(1) \ "errorStatus").extract[String] shouldEqual "SUCCESS"
-          (executionsData(1) \ "startTime").extract[String] shouldEqual "2023-12-01T14:37:05.577+03:00"
-          (executionsData(1) \ "mappingUrls").extract[Seq[String]].length shouldEqual 21
-          (executionsData(1) \ "mappingUrls")(10).extract[String] shouldEqual "https://aiccelerate.eu/fhir/mappings/pilot1/medication-administration-mapping"
+          (executionsData(1) \ "startTime").extract[String] shouldEqual "2023-12-01T14:37:31.348+03:00"
+          (executionsData(1) \ "mappingUrls").extract[Seq[String]].length shouldEqual 1
+          (executionsData(1) \ "mappingUrls")(0).extract[String] shouldEqual "https://aiccelerate.eu/fhir/mappings/pilot1/other-observation-mapping"
         })
     }
 
@@ -87,29 +87,29 @@ class ExecutionServiceTest extends AsyncWordSpec with Matchers with BeforeAndAft
         .map(execution => {
           (execution \ "id").extract[String] shouldEqual "4df92d34-7712-47a7-8677-15339797c554"
           (execution \ "errorStatus").extract[String] shouldEqual "PARTIAL_SUCCESS"
-          (execution \ "mappingUrls").extract[Seq[String]].length shouldEqual 21
-          (execution \ "mappingUrls")(0).extract[String] shouldEqual "https://aiccelerate.eu/fhir/mappings/pilot1/patient-reported-conditions-mapping"
+          (execution \ "mappingUrls").extract[Seq[String]].length shouldEqual 6
+          (execution \ "mappingUrls")(0).extract[String] shouldEqual "https://aiccelerate.eu/fhir/mappings/pilot1/preoperative-risks-mapping"
         })
     }
 
     "should get execution logs by execution id" in {
       executionService.getExecutionLogs("4df92d34-7712-47a7-8677-15339797c554")
         .map(logs => {
-          logs.length shouldEqual 42
+          logs.length shouldEqual 11
 
           // iterate over logs and check project ids/job ids/execution ids are correct
-          (0 to 41).foreach(i => {
+          (0 to 10).foreach(i => {
             (logs(i) \ "projectId").extract[String] shouldEqual "pilot1"
             (logs(i) \ "executionId").extract[String] shouldEqual "4df92d34-7712-47a7-8677-15339797c554"
             (logs(i) \ "jobId").extract[String] shouldEqual "pilot1"
           })
 
           // check the first log -> STARTED
-          (logs(0) \ "result").extract[String] shouldEqual "STARTED"
-          (logs(0) \ "mappingUrl").extract[String] shouldEqual "https://aiccelerate.eu/fhir/mappings/pilot1/patient-mapping"
+          (logs.head \ "result").extract[String] shouldEqual "STARTED"
+          (logs.head \ "mappingUrl").extract[String] shouldEqual "https://aiccelerate.eu/fhir/mappings/pilot1/medication-administration-mapping"
           (logs(1) \ "result").extract[String] shouldEqual "SUCCESS"
-          (logs(1) \ "numOfFhirResources").extract[Int] shouldEqual 10
-          (logs(1) \ "mappingUrl").extract[String] shouldEqual "https://aiccelerate.eu/fhir/mappings/pilot1/patient-mapping"
+          (logs(1) \ "numOfFhirResources").extract[Int] shouldEqual 5
+          (logs(1) \ "mappingUrl").extract[String] shouldEqual "https://aiccelerate.eu/fhir/mappings/pilot1/medication-administration-mapping"
 
         })
     }
