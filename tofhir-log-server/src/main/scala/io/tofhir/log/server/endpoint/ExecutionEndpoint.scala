b3cbdf0f2b8ceadda18dcff02b2bd2ad8619a094
okanmercan <okanmercan16@gmail.com>
2023-12-19 11:42:49 +0300

    :refactor: recycle: WebServerConfig is moved to tofhir-server-common.

--- tofhir-log-server/src/main/scala/io/tofhir/log/server/endpoint/ExecutionEndpoint.scala
+++ tofhir-log-server/src/main/scala/io/tofhir/log/server/endpoint/ExecutionEndpoint.scala
@@ -5,10 +5,10 @@ import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
 import io.tofhir.log.server.model.Json4sSupport._
 import com.typesafe.scalalogging.LazyLogging
-import io.tofhir.log.server.config.WebServerConfig
 import io.tofhir.log.server.interceptor.{ICORSHandler, IErrorHandler}
 import io.tofhir.log.server.service.ExecutionService
 import ExecutionEndpoint.{SEGMENT_EXECUTIONS, SEGMENT_JOB, SEGMENT_LOGS, SEGMENT_PROJECTS}
+import io.tofhir.server.common.config.WebServerConfig
 
 class ExecutionEndpoint(webServerConfig: WebServerConfig) extends ICORSHandler with IErrorHandler with LazyLogging {
 
