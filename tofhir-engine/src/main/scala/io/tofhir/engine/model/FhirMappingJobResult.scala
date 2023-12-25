8a4101b3dfe1a12cd0ea1b5564864b7637e384ea
dogukan10 <dogukan10cav@gmail.com>
2023-12-25 11:25:45 +0300

    :bug: fix: Add 'status' property to FhirMappingJobResult to handle errors where it does not have any numerical metrics related to the result

--- tofhir-engine/src/main/scala/io/tofhir/engine/model/FhirMappingJobResult.scala
+++ tofhir-engine/src/main/scala/io/tofhir/engine/model/FhirMappingJobResult.scala
@@ -12,22 +12,34 @@ import net.logstash.logback.marker.Markers.{append}
  * @param numOfNotMapped      Total number of mappings per row that is not successful
  * @param numOfFhirResources  Total number of FHIR resources created as a result mapping(s)
  * @param numOfFailedWrites   Total number of FHIR resources that cannot be written to the configured sink (e.g. FHIR repository)
+ * @param status              An optional status indicating the overall outcome of the mapping job.
  */
 case class FhirMappingJobResult(mappingJobExecution: FhirMappingJobExecution,
                                 mappingUrl: Option[String],
                                 numOfInvalids: Long = -1,
                                 numOfNotMapped: Long = -1,
                                 numOfFhirResources: Long = -1,
-                                numOfFailedWrites: Long = -1
+                                numOfFailedWrites: Long = -1,
+                                status: Option[String] = None
                                ) {
   final val eventId: String = "MAPPING_JOB_RESULT"
-  val result: String = (numOfInvalids + numOfNotMapped + numOfFailedWrites) match {
-    case -3 => "STARTED"
-    case 0 => "SUCCESS"
-    case _ => // Check if it is complete failure or there are some successfully written resources
-      numOfFhirResources match {
-        case 0 => "FAILURE"
-        case _ => "PARTIAL_SUCCESS"
+  /**
+   * Determines the status of a FHIR mapping job. If it has an associated status, it is returned.
+   * Otherwise, the status is determined using the result of the mapping job i.e. the number of
+   * invalid rows, the number of failed writes etc.
+   */
+  val result: String = {
+    if(status.nonEmpty)
+      status.get
+    else
+      numOfInvalids + numOfNotMapped + numOfFailedWrites match {
+        case -3 => FhirMappingJobResult.STARTED
+        case 0 => FhirMappingJobResult.SUCCESS
+        case _ => // Check if it is complete failure or there are some successfully written resources
+          numOfFhirResources match {
+            case 0 => FhirMappingJobResult.FAILURE
+            case _ => FhirMappingJobResult.PARTIAL_SUCCESS
+          }
       }
   }
 
@@ -55,4 +67,34 @@ case class FhirMappingJobResult(mappingJobExecution: FhirMappingJobExecution,
                     .and(append("numOfFailedWrites", numOfFailedWrites)
                       .and(append("eventId", eventId))))))))))
   }
+}
+
+/**
+ * Object representing possible statuses of a FHIR mapping job.
+ */
+object FhirMappingJobResult {
+  /**
+   * Represents the status when the mapping job has been started.
+   * Resources are in the process of being mapped and written.
+   */
+  val STARTED: String = "STARTED"
+
+  /**
+   * Represents the status when the mapping job has failed.
+   * No FHIR Resources are written because the mapping or writing process has encountered a failure.
+   */
+  val FAILURE: String = "FAILURE"
+
+  /**
+   * Represents the status when the mapping job has achieved partial success.
+   * Some of the FHIR Resources are written to the configured FHIR Repository or File System,
+   * while some have encountered failures during the process.
+   */
+  val PARTIAL_SUCCESS: String = "PARTIAL_SUCCESS"
+
+  /**
+   * Represents the status when the mapping job has successfully completed.
+   * All FHIR Resources are written to the configured FHIR Repository or File System.
+   */
+  val SUCCESS: String = "SUCCESS"
 }
