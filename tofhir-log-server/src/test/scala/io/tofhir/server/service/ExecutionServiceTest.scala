fb152ad8877ceac29e01ccb8fb25747dd8593741
dogukan10 <dogukan10cav@gmail.com>
2023-12-26 09:51:15 +0300

    :recycle: refactor: Remove redundant processing of mapping task logs in the execution service

--- tofhir-log-server/src/test/scala/io/tofhir/server/service/ExecutionServiceTest.scala
+++ tofhir-log-server/src/test/scala/io/tofhir/server/service/ExecutionServiceTest.scala
@@ -1,107 +1,115 @@
 package io.tofhir.server.service
 
 import io.tofhir.engine.Execution.actorSystem.dispatcher
+import io.tofhir.engine.model.FhirMappingJobResult
 import io.tofhir.log.server.model.Json4sSupport.formats
 import io.tofhir.log.server.service.ExecutionService
+import org.json4s.JsonAST.JArray
 import org.scalatest.BeforeAndAfterAll
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AsyncWordSpec
 
+/**
+ * Tests for the ExecutionService.
+ *
+ * This suite focuses on testing the functionality of the ExecutionService, which works with the log file provided
+ * in the test/resources/log-sample.log file. The log file includes records for the following scenarios:
+ *  - An execution of a mapping job with one mapping, which fails due to an incorrect FHIR Repository URL.
+ *  - An execution of a mapping job with one mapping, which completes successfully.
+ */
 class ExecutionServiceTest extends AsyncWordSpec with Matchers with BeforeAndAfterAll {
 
   val executionService: ExecutionService = new ExecutionService
+  // The identifier for the project
+  private val projectId = "pilot1"
+  // The identifier for the specific job
+  private val jobId = "pilot1-preop"
+  // The identifier for the first mapping job execution
+  private val firstMappingJobExecutionId = "b9d60a61-6d0b-4ea1-9c4e-84e343334b14"
+  // The identifier for the second mapping job execution
+  private val secondMappingJobExecutionId = "70503f99-8aea-4b4f-8b1b-6dbc3d5509fe"
 
   "The Execution Service" should {
-    "should get executions" in {
-      executionService.getExecutions("pilot1", "pilot1", Map.empty)
+
+    "should get mapping job executions" in {
+      executionService.getExecutions(projectId, jobId, Map.empty)
         .map(executions => {
           val executionsData = executions._1
           val count = executions._2
-          count shouldEqual 8
+          count shouldEqual 2
           executionsData.length shouldEqual count
-          (executionsData.head \ "id").extract[String] shouldEqual "1f47ee28-65b8-48b5-bbd6-347b15254cfb"
-          (executionsData.head \ "errorStatus").extract[String] shouldEqual "SUCCESS"
-          (executionsData.head \ "mappingUrls").extract[Seq[String]].length shouldEqual 4
+          (executionsData.head \ "id").extract[String] shouldEqual secondMappingJobExecutionId
+          (executionsData.head \ "errorStatus").extract[String] shouldEqual FhirMappingJobResult.SUCCESS
+          (executionsData.head \ "mappingUrls").extract[Seq[String]].length shouldEqual 1
 
-          (executionsData(5) \ "id").extract[String] shouldEqual "4df92d34-7712-47a7-8677-15339797c554"
-          (executionsData(5) \ "errorStatus").extract[String] shouldEqual "PARTIAL_SUCCESS"
-          (executionsData(5) \ "mappingUrls").extract[Seq[String]].length shouldEqual 6
-
-          (executionsData(5) \ "mappingUrls")(1).extract[String] shouldEqual "https://aiccelerate.eu/fhir/mappings/pilot1/health-behavior-assessment-mapping"
+          (executionsData(1) \ "id").extract[String] shouldEqual firstMappingJobExecutionId
+          (executionsData(1) \ "errorStatus").extract[String] shouldEqual FhirMappingJobResult.FAILURE
+          (executionsData(1) \ "mappingUrls").extract[Seq[String]].length shouldEqual 1
         })
     }
 
     "should get executions filtered by a date range" in {
-      val queryParams = Map("dateBefore" -> "2023-12-06", "dateAfter" -> "2023-12-03")
-      executionService.getExecutions("pilot1", "pilot1", queryParams)
+      val queryParams = Map("dateBefore" -> "2023-12-27", "dateAfter" -> "2023-12-26")
+      executionService.getExecutions(projectId, jobId, queryParams)
         .map(executions => {
           val executionsData = executions._1
           val count = executions._2
           count shouldEqual 1
           executionsData.length shouldEqual count
-          (executionsData.head \ "id").extract[String] shouldEqual "1f47ee28-65b8-48b5-bbd6-347b15254cfb"
-          (executionsData.head \ "errorStatus").extract[String] shouldEqual "SUCCESS"
-          (executionsData.head \ "mappingUrls").extract[Seq[String]].length shouldEqual 4
-          (executionsData.head \ "mappingUrls")(1).extract[String] shouldEqual "https://aiccelerate.eu/fhir/mappings/pilot1/vital-signs-mapping"
+          (executionsData.head \ "id").extract[String] shouldEqual secondMappingJobExecutionId
+          (executionsData.head \ "errorStatus").extract[String] shouldEqual FhirMappingJobResult.SUCCESS
+          (executionsData.head \ "mappingUrls").extract[Seq[String]].length shouldEqual 1
+          (executionsData.head \ "mappingUrls")(0).extract[String] shouldEqual "https://aiccelerate.eu/fhir/mappings/pilot1/patient-mapping"
         })
     }
 
     "should not get any execution when page number is greater than the maximum" in {
-      val queryParams = Map("dateBefore" -> "2023-12-06", "dateAfter" -> "2023-12-03", "rowsPerPage" -> "4", "page" -> "3")
-      executionService.getExecutions("pilot1", "pilot1", queryParams)
+      val queryParams = Map("dateBefore" -> "2023-12-27", "dateAfter" -> "2023-12-03", "rowsPerPage" -> "4", "page" -> "3")
+      executionService.getExecutions(projectId, jobId, queryParams)
         .map(executions => {
           val executionsData = executions._1
           val count = executions._2
           count shouldEqual 0
           executionsData.length shouldEqual count
-          })
+        })
     }
 
     "should get executions filtered by error status" in {
-      val queryParams = Map("errorStatuses" -> "SUCCESS")
-      executionService.getExecutions("pilot1", "pilot1", queryParams)
+      val queryParams = Map("errorStatuses" -> FhirMappingJobResult.SUCCESS)
+      executionService.getExecutions(projectId, jobId, queryParams)
         .map(executions => {
           val executionsData = executions._1
           val count = executions._2
-          count shouldEqual 3
+          count shouldEqual 1
           executionsData.length shouldEqual count
-          (executionsData(1) \ "id").extract[String] shouldEqual "57bb4431-5d58-44d5-b525-a838c0cfa21c"
-          (executionsData(1) \ "errorStatus").extract[String] shouldEqual "SUCCESS"
-          (executionsData(1) \ "startTime").extract[String] shouldEqual "2023-12-01T14:37:31.348+03:00"
-          (executionsData(1) \ "mappingUrls").extract[Seq[String]].length shouldEqual 1
-          (executionsData(1) \ "mappingUrls")(0).extract[String] shouldEqual "https://aiccelerate.eu/fhir/mappings/pilot1/other-observation-mapping"
+          (executionsData.head \ "id").extract[String] shouldEqual secondMappingJobExecutionId
+          (executionsData.head \ "errorStatus").extract[String] shouldEqual FhirMappingJobResult.SUCCESS
+          (executionsData.head \ "mappingUrls").extract[Seq[String]].length shouldEqual 1
         })
     }
 
     "should get execution by id" in {
-      executionService.getExecutionById("pilot1", "pilot1", "4df92d34-7712-47a7-8677-15339797c554")
+      executionService.getExecutionById(projectId, jobId, secondMappingJobExecutionId)
         .map(execution => {
-          (execution \ "id").extract[String] shouldEqual "4df92d34-7712-47a7-8677-15339797c554"
-          (execution \ "errorStatus").extract[String] shouldEqual "PARTIAL_SUCCESS"
-          (execution \ "mappingUrls").extract[Seq[String]].length shouldEqual 6
-          (execution \ "mappingUrls")(0).extract[String] shouldEqual "https://aiccelerate.eu/fhir/mappings/pilot1/preoperative-risks-mapping"
+          (execution \ "id").extract[String] shouldEqual secondMappingJobExecutionId
+          (execution \ "errorStatus").extract[String] shouldEqual FhirMappingJobResult.SUCCESS
+          (execution \ "mappingUrls").extract[Seq[String]].length shouldEqual 1
+          (execution \ "mappingUrls")(0).extract[String] shouldEqual "https://aiccelerate.eu/fhir/mappings/pilot1/patient-mapping"
         })
     }
 
     "should get execution logs by execution id" in {
-      executionService.getExecutionLogs("4df92d34-7712-47a7-8677-15339797c554")
+      executionService.getExecutionLogs(firstMappingJobExecutionId)
         .map(logs => {
-          logs.length shouldEqual 11
-
-          // iterate over logs and check project ids/job ids/execution ids are correct
-          (0 to 10).foreach(i => {
-            (logs(i) \ "projectId").extract[String] shouldEqual "pilot1"
-            (logs(i) \ "executionId").extract[String] shouldEqual "4df92d34-7712-47a7-8677-15339797c554"
-            (logs(i) \ "jobId").extract[String] shouldEqual "pilot1"
-          })
+          logs.length shouldEqual 2
 
           // check the first log -> STARTED
-          (logs.head \ "result").extract[String] shouldEqual "STARTED"
-          (logs.head \ "mappingUrl").extract[String] shouldEqual "https://aiccelerate.eu/fhir/mappings/pilot1/medication-administration-mapping"
-          (logs(1) \ "result").extract[String] shouldEqual "SUCCESS"
-          (logs(1) \ "numOfFhirResources").extract[Int] shouldEqual 5
-          (logs(1) \ "mappingUrl").extract[String] shouldEqual "https://aiccelerate.eu/fhir/mappings/pilot1/medication-administration-mapping"
+          (logs.head \ "result").extract[String] shouldEqual FhirMappingJobResult.STARTED
+          (logs.head \ "error_logs").extract[JArray].arr.size shouldEqual 10
 
+          // check the second log -> FAILURE
+          (logs(1) \ "result").extract[String] shouldEqual FhirMappingJobResult.FAILURE
+          (logs(1) \ "numOfFailedWrites").extract[String] shouldEqual "10"
         })
     }
   }
