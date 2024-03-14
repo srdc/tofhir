815a726ad171653e059572f37c68bed92a29ff6a
dogukan10 <dogukan10cav@gmail.com>
2024-03-14 10:54:30 +0300

    :sparkles: Add authentication options for Fhir Server Data Source

--- tofhir-engine/src/main/scala/io/tofhir/engine/data/read/FhirServerDataSourceReader.scala
+++ tofhir-engine/src/main/scala/io/tofhir/engine/data/read/FhirServerDataSourceReader.scala
@@ -1,6 +1,7 @@
 package io.tofhir.engine.data.read
 
-import io.tofhir.engine.model.{FhirServerSource, FhirServerSourceSettings}
+import io.onfhir.spark.reader.FhirApiReader.OPTIONS
+import io.tofhir.engine.model.{BasicAuthenticationSettings, BearerTokenAuthorizationSettings, FhirServerSource, FhirServerSourceSettings, FixedTokenAuthenticationSettings}
 import org.apache.spark.sql.types.StructType
 import org.apache.spark.sql.{DataFrame, SparkSession}
 
@@ -32,6 +33,8 @@ class FhirServerDataSourceReader(spark: SparkSession) extends BaseDataSourceRead
    * @throws NotImplementedError      If the specified source format is not implemented.
    */
   override def read(mappingSource: FhirServerSource, sourceSettings: FhirServerSourceSettings, schema: Option[StructType], timeRange: Option[(LocalDateTime, LocalDateTime)], limit: Option[Int] = Option.empty, jobId: Option[String] = Option.empty): DataFrame = {
+    // extract Spark option for the authentication from the given source settings
+    val authenticationOptions = extractAuthenticationOptions(sourceSettings)
     // read data from a FHIR Server using a custom Spark data source i.e. io.onfhir.spark.reader.FhirApiTableProvider
     spark
       .read
@@ -39,6 +42,39 @@ class FhirServerDataSourceReader(spark: SparkSession) extends BaseDataSourceRead
       .option("url", sourceSettings.serverUrl)
       .option("rtype", mappingSource.resourceType)
       .option("query", s"?${mappingSource.query.getOrElse("")}")
+      .options(authenticationOptions)
       .load()
+  }
+
+  /**
+   * Extracts authentication options from the provided FhirServerSourceSettings.
+   *
+   * @param sourceSettings The FhirServerSourceSettings containing security settings.
+   * @return A map of authentication options extracted from the source settings.
+   */
+  private def extractAuthenticationOptions(sourceSettings: FhirServerSourceSettings): Map[String, String] = {
+    sourceSettings.securitySettings
+      .map {
+        case BearerTokenAuthorizationSettings(clientId, clientSecret, requiredScopes, authzServerTokenEndpoint, clientAuthenticationMethod) =>
+          Map(
+            OPTIONS.AUTH -> OPTIONS.AUTH_TYPE.BEARER_TOKEN,
+            OPTIONS.AUTH_BEARER_TOKEN_PARAMETERS.CLIENT_ID -> clientId,
+            OPTIONS.AUTH_BEARER_TOKEN_PARAMETERS.CLIENT_SECRET -> clientSecret,
+            OPTIONS.AUTH_BEARER_TOKEN_PARAMETERS.REQUIRED_SCOPES -> requiredScopes.mkString(","),
+            OPTIONS.AUTH_BEARER_TOKEN_PARAMETERS.AUTHZ_SERVER_TOKEN_ENDPOINT -> authzServerTokenEndpoint,
+            OPTIONS.AUTH_BEARER_TOKEN_PARAMETERS.CLIENT_AUTHENTICATION_METHOD -> clientAuthenticationMethod
+          )
+        case BasicAuthenticationSettings(username, password) =>
+          Map(
+            OPTIONS.AUTH -> OPTIONS.AUTH_TYPE.BASIC,
+            OPTIONS.AUTH_BASIC_PARAMETERS.USERNAME -> username,
+            OPTIONS.AUTH_BASIC_PARAMETERS.PASSWORD -> password
+          )
+        case FixedTokenAuthenticationSettings(token) =>
+          Map(
+            OPTIONS.AUTH -> OPTIONS.AUTH_TYPE.FIXED_TOKEN,
+            OPTIONS.AUTH_FIXED_TOKEN_PARAMETERS.TOKEN -> token
+          )
+      }.getOrElse(Map.empty)
   }
 }
