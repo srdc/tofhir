5deba4c793a7069c406e3fc072b8b2028f612ded
dogukan10 <dogukan10cav@gmail.com>
2024-03-26 10:31:17 +0300

    :sparkles: Implement an API to import a schema from a Fhir Server

---
+++ tofhir-server/src/main/scala/io/tofhir/server/model/ImportSchemaSettings.scala
@@ -0,0 +1,12 @@
+package io.tofhir.server.model
+
+import io.tofhir.engine.model.IFhirRepositorySecuritySettings
+
+/**
+ * Represents the settings required for importing a schema from a FHIR server.
+ *
+ * @param baseUrl The base URL of the FHIR server from which to import the schema.
+ * @param resourceId The ID of the resource representing the schema i.e. StructureDefinition to be imported.
+ * @param securitySettings Optional security settings for accessing the FHIR server, if applicable.
+ */
+case class ImportSchemaSettings(baseUrl: String, resourceId: String, securitySettings: Option[IFhirRepositorySecuritySettings])
