3a23226bc2ae94c79edf8d8fb47ef3e079bd6870
dogukan10 <dogukan10cav@gmail.com>
2024-05-21 11:38:00 +0300

    :recycle: refactor: Utilize a singleton onFhir client in tests

--- tofhir-engine/src/test/scala/io/tofhir/OnFhirTestContainer.scala
+++ tofhir-engine/src/test/scala/io/tofhir/OnFhirTestContainer.scala
@@ -1,41 +1,47 @@
 package io.tofhir
 
-import io.tofhir.engine.Execution.actorSystem
 import io.onfhir.client.OnFhirNetworkClient
+import io.tofhir.engine.Execution.actorSystem
 import org.testcontainers.containers.GenericContainer
 import org.testcontainers.containers.wait.strategy.Wait
-import org.testcontainers.junit.jupiter.Container
 import org.testcontainers.utility.DockerImageName
 
+import java.time.Duration
+
 /**
- * Initializer of the onFHIR test containers used in toFHIR testing.
+ * Singleton object responsible for initializing and managing the OnFHIR test container used in toFHIR testing.
+ * The container is configured with the necessary environment variables and started with specified settings.
+ * Provides a method to obtain an OnFhirNetworkClient instance for interacting with the OnFHIR container.
  */
-trait OnFhirTestContainer {
-//
-//  implicit val actorSystem: ActorSystem = ActorSystem("toFhirEngineTest") // required for OnFhirNetworkClient
+object OnFhirTestContainer {
+  private val ONFHIR_TESTCONTAINER_PORT = 8981 // Port number of the OnFhir instance to be created for testing.
+  private val TIMEOUT_SECONDS = 180 // Timeout in seconds for container startup
 
-  val onFhirClient = initializeOnFhirClient
-
-  /**
-   * Deploy an onFhir container for testing purpose and return its client.
-   * @param actorSystem
-   * @return
-   */
-  def initializeOnFhirClient: OnFhirNetworkClient = {
-    val ONFHIR_TESTCONTAINER_PORT = 8981 // Port number of the onfhir instance to be created for testing.
-
-    @Container
+  private lazy val container: GenericContainer[_] = {
     val container: GenericContainer[Nothing] = new GenericContainer(DockerImageName.parse("srdc/onfhir:r4")).withExposedPorts(ONFHIR_TESTCONTAINER_PORT)
-
     container.addEnv("DB_EMBEDDED", "true")
     container.addEnv("SERVER_PORT", ONFHIR_TESTCONTAINER_PORT.toString)
     container.addEnv("SERVER_BASE_URI", "fhir")
     container.addEnv("FHIR_ROOT_URL", s"http://${container.getHost}:$ONFHIR_TESTCONTAINER_PORT/fhir")
     container.withReuse(true)
-    container.waitingFor(Wait.forHttp("/fhir").forStatusCode(200))
+    container.waitingFor(Wait.forHttp("/fhir").forStatusCode(200).withStartupTimeout(Duration.ofSeconds(TIMEOUT_SECONDS)))
     container.start()
-
-    OnFhirNetworkClient.apply(s"http://${container.getHost}:${container.getFirstMappedPort}/fhir")
+    container
   }
 
+  /**
+   * Retrieves an OnFhirNetworkClient instance for interacting with the OnFHIR container.
+   *
+   * @return The singleton onFhirNetworkClient.
+   */
+  def getOnFhirClient: OnFhirNetworkClient = {
+    OnFhirNetworkClient(s"http://localhost:${container.getMappedPort(ONFHIR_TESTCONTAINER_PORT)}/fhir")
+  }
+}
+
+/**
+ * Trait providing access to the OnFHIR test client.
+ */
+trait OnFhirTestContainer {
+  val onFhirClient: OnFhirNetworkClient = OnFhirTestContainer.getOnFhirClient
 }
--- tofhir-engine/src/test/scala/io/tofhir/test/FhirServerSourceTest.scala
+++ tofhir-engine/src/test/scala/io/tofhir/test/FhirServerSourceTest.scala
@@ -4,7 +4,6 @@ import akka.http.scaladsl.model.StatusCodes
 import io.onfhir.api.Resource
 import io.onfhir.api.client.FhirBatchTransactionRequestBuilder
 import io.onfhir.api.util.FHIRUtil
-import io.onfhir.client.OnFhirNetworkClient
 import io.onfhir.path.FhirPathUtilFunctionsFactory
 import io.onfhir.util.JsonFormatter._
 import io.tofhir.{OnFhirTestContainer, ToFhirTestSpec}
@@ -24,15 +23,12 @@ class FhirServerSourceTest extends AsyncFlatSpec with BeforeAndAfterAll with ToF
 
   // Sink Settings of mapping job
   val fhirSinkSettings: FhirRepositorySinkSettings = FhirRepositorySinkSettings(fhirRepoUrl = onFhirClient.getBaseUrl())
-  // Define OnFhir clients for source and target servers
-  val targetOnFhirClient: OnFhirNetworkClient = onFhirClient
-  val sourceOnFhirClient: OnFhirNetworkClient = initializeOnFhirClient // Initialize another OnFhirClient
 
   // Settings of Fhir Server data source
   val fhirServerSourceSettings: Map[String, FhirServerSourceSettings] =
     Map(
       "source" ->
-        FhirServerSourceSettings(name = "test-fhir-server-source", sourceUri = "https://test-data", serverUrl = sourceOnFhirClient.getBaseUrl())
+        FhirServerSourceSettings(name = "test-fhir-server-source", sourceUri = "https://test-data", serverUrl = onFhirClient.getBaseUrl())
     )
 
   // FhirMappingJobManager to execute mapping job
@@ -51,15 +47,15 @@ class FhirServerSourceTest extends AsyncFlatSpec with BeforeAndAfterAll with ToF
     dataProcessingSettings = DataProcessingSettings()
   )
 
-  // Observation resource to be created on the source onFhir server
+  // Observation resource to be created on onFhir server as the source data
   val testObservationResource: Resource = JsonMethods.parse(Source.fromInputStream(getClass.getResourceAsStream("/fhir-resources/observation-resource.json")).mkString).extract[Resource]
 
   /**
-   * Create "example-observation" resource on the source onFHIR server before starting the tests.
+   * Create "example-observation" resource on the onFHIR server before starting the tests.
    * */
   override protected def beforeAll(): Unit = {
     super.beforeAll()
-    sourceOnFhirClient.batch()
+    onFhirClient.batch()
       .entry(_.update(testObservationResource))
       .returnMinimal().asInstanceOf[FhirBatchTransactionRequestBuilder].execute() map { res =>
       res.httpStatus shouldBe StatusCodes.OK
@@ -67,11 +63,11 @@ class FhirServerSourceTest extends AsyncFlatSpec with BeforeAndAfterAll with ToF
   }
 
   /**
-   * After the tests complete, delete the "example-observation" resource on the source onFHIR server.
+   * After the tests complete, delete the "example-observation" resource on the onFHIR server.
    * */
   override protected def afterAll(): Unit = {
     super.afterAll()
-    sourceOnFhirClient.batch()
+    onFhirClient.batch()
       .entry(_.delete("Observation", "example-observation"))
       .returnMinimal().asInstanceOf[FhirBatchTransactionRequestBuilder].execute() map { res =>
       res.httpStatus shouldBe StatusCodes.OK
@@ -79,7 +75,7 @@ class FhirServerSourceTest extends AsyncFlatSpec with BeforeAndAfterAll with ToF
   }
 
   /**
-   * Tests the mapping job which reads data from the source onFHIR and executes the mapping on this data.
+   * Tests the mapping job which reads data from the onFHIR and executes the mapping on this data.
    * It should produce two observation resources.
    * */
   "Observation mapping" should "should read data from Fhir Server source and map it" in {
@@ -105,17 +101,18 @@ class FhirServerSourceTest extends AsyncFlatSpec with BeforeAndAfterAll with ToF
   }
 
   /**
-   * Executes the mapping job which reads data from the source onFHIR and writes the generated resources to target onFHIR.
-   * There should be two Observation resources on the target onFHIR after the mapping job is completed.
-   * */
+   * Executes the mapping job, which reads data from OnFHIR and writes the generated resources back to OnFHIR.
+   * After the mapping job is completed, there should be three Observation resources on OnFHIR: one Observation resource
+   * as the source data and two Observation resources as the generated FHIR resources.
+   */
   it should "map test data and write it to FHIR repo successfully" in {
     fhirMappingJobManager
       .executeMappingJob(mappingJobExecution = FhirMappingJobExecution(mappingTasks = Seq(observationMappingTask), job = fhirMappingJob), sourceSettings = fhirServerSourceSettings, sinkSettings = fhirSinkSettings)
       .flatMap(_ => {
-        targetOnFhirClient.search("Observation").where("subject", "Patient/example-patient-fhir")
+        onFhirClient.search("Observation").where("subject", "Patient/example-patient-fhir")
           .executeAndReturnBundle() flatMap { obsBundle =>
-          obsBundle.searchResults.size shouldBe 2
-          var batchRequest: FhirBatchTransactionRequestBuilder = targetOnFhirClient.batch()
+          obsBundle.searchResults.size shouldBe 3
+          var batchRequest: FhirBatchTransactionRequestBuilder = onFhirClient.batch()
           obsBundle.searchResults.foreach(obs =>
             batchRequest = batchRequest.entry(_.delete("Observation", (obs \ "id").extract[String]))
           )
