d65a7138b0cbfa6a7e6f9be6e1659ae1ad43692c
A. Anil Sinaci <anil@srdc.com.tr>
2024-09-26 13:10:51 +0300

    :sparkles: Add a description field to mapping job definitions.

--- tofhir-engine/src/main/scala/io/tofhir/engine/model/FhirMappingJob.scala
+++ tofhir-engine/src/main/scala/io/tofhir/engine/model/FhirMappingJob.scala
@@ -9,27 +9,30 @@ import javax.ws.rs.BadRequestException
 /**
  * A mapping job including one or more mapping tasks from a configured data source to a configured sink
  *
- * @param id                            Unique identifier for the mapping job
- * @param sourceSettings                Settings for data source system(s)
- * @param sinkSettings                  Settings for sink to write the mapped FHIR resources
- * @param terminologyServiceSettings    Settings for a terminology service that will be used within mappings
- * @param identityServiceSettings       Settings for a external identity service that will be used within mappings
- * @param mappings                      Mapping tasks
- * @param schedulingSettings            Scheduling information for periodic mapping jobs
- * @param dataProcessingSettings        Settings for data processing(e.g. archiveMode etc.)
- * @param useFhirSinkAsIdentityService  If true it means the FHIR repository to write the mapped resources where the configuration
- *                                      is given in sink settings will be used as identity service (Override identityServiceSettings if given)
+ * @param id                           Unique identifier for the mapping job
+ * @param name                         Name of the mapping job
+ * @param description                  Description of the mapping job
+ * @param sourceSettings               Settings for data source system(s)
+ * @param sinkSettings                 Settings for sink to write the mapped FHIR resources
+ * @param terminologyServiceSettings   Settings for a terminology service that will be used within mappings
+ * @param identityServiceSettings      Settings for a external identity service that will be used within mappings
+ * @param mappings                     Mapping tasks
+ * @param schedulingSettings           Scheduling information for periodic mapping jobs
+ * @param dataProcessingSettings       Settings for data processing(e.g. archiveMode etc.)
+ * @param useFhirSinkAsIdentityService If true it means the FHIR repository to write the mapped resources where the configuration
+ *                                     is given in sink settings will be used as identity service (Override identityServiceSettings if given)
  */
 case class FhirMappingJob(id: String = UUID.randomUUID().toString,
                           name: Option[String] = None,
-                          sourceSettings: Map[String,MappingJobSourceSettings],
+                          description: Option[String] = None,
+                          sourceSettings: Map[String, MappingJobSourceSettings],
                           sinkSettings: FhirSinkSettings,
-                          terminologyServiceSettings:Option[TerminologyServiceSettings] = None,
+                          terminologyServiceSettings: Option[TerminologyServiceSettings] = None,
                           identityServiceSettings: Option[IdentityServiceSettings] = None,
                           mappings: Seq[FhirMappingTask],
                           schedulingSettings: Option[BaseSchedulingSettings] = None,
                           dataProcessingSettings: DataProcessingSettings = DataProcessingSettings(),
-                          useFhirSinkAsIdentityService:Boolean = false
+                          useFhirSinkAsIdentityService: Boolean = false
                          ) {
   /**
    * Validates the mapping job
@@ -38,31 +41,33 @@ case class FhirMappingJob(id: String = UUID.randomUUID().toString,
    *                             by a mapping task is missing from the source settings.
    */
   def validate(): Unit = {
-    if(sourceSettings.exists(_._2.asStream) && schedulingSettings.nonEmpty){
+    if (sourceSettings.exists(_._2.asStream) && schedulingSettings.nonEmpty) {
       throw new BadRequestException("Streaming jobs cannot be scheduled.")
     }
 
     // Check names of the mappingTasks, if a duplicate name is found, throw an error
     val duplicateMappingTasks = FhirMappingJobFormatter.findDuplicateMappingTaskNames(mappings)
-    if(duplicateMappingTasks.nonEmpty){
+    if (duplicateMappingTasks.nonEmpty) {
       throw new BadRequestException(s"Duplicate MappingTask name(s) found: ${duplicateMappingTasks.mkString(", ")}. Each MappingTask must have a unique name.")
     }
 
     // Check mapping tasks of the job, if a data source of a mapping task is missing, throw an error
     mappings.foreach(mappingTask => {
       mappingTask.sourceBinding.foreach(sourceBinding => {
-        if(sourceBinding._2.sourceRef.nonEmpty && !sourceSettings.contains(sourceBinding._2.sourceRef.get)) {
+        if (sourceBinding._2.sourceRef.nonEmpty && !sourceSettings.contains(sourceBinding._2.sourceRef.get)) {
           throw new BadRequestException(s"The data source referenced by source name '${sourceBinding._2.sourceRef.get}' in the mapping task '${mappingTask.name}' is not found in the source settings of the job.")
         }
       })
     })
   }
+
   /**
    * Return the final identity service settings
+   *
    * @return
    */
-  def getIdentityServiceSettings():Option[IdentityServiceSettings] =
-    if(useFhirSinkAsIdentityService &&sinkSettings.isInstanceOf[FhirRepositorySinkSettings])
+  def getIdentityServiceSettings(): Option[IdentityServiceSettings] =
+    if (useFhirSinkAsIdentityService && sinkSettings.isInstanceOf[FhirRepositorySinkSettings])
       Some(sinkSettings.asInstanceOf[FhirRepositorySinkSettings])
     else identityServiceSettings
 
