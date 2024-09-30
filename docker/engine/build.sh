fb97f207623231388ca565db4f85f450c4f93707
dogukan10 <dogukan10cav@gmail.com>
2024-09-30 09:19:25 +0300

    :sparkles: Upgrade Fluentd to v1.17-1

--- docker/engine/build.sh
+++ docker/engine/build.sh
@@ -1,5 +1,5 @@
 # Execute one of the following commands from the project.root.directory (../../)
 # Do not forget to use the correct version while tagging.
 
-docker build -f docker/engine/Dockerfile-addJar -t srdc/tofhir-engine:latest
-docker build -f docker/engine/Dockerfile-buildJar -t srdc/tofhir-engine:latest
+docker build -f docker/engine/Dockerfile-addJar -t srdc/tofhir-engine:latest .
+docker build -f docker/engine/Dockerfile-buildJar -t srdc/tofhir-engine:latest .
