60635f266c75081698100992b337b6405c43212e
emrecam <cam.emre090@gmail.com>
2024-09-30 19:08:56 +0300

    :recycle: Refactor: Use FHIR_DATA_TYPES instead of hardcoded values of data types

--- tofhir-server/src/test/scala/io/tofhir/server/endpoint/SchemaEndpointTest.scala
+++ tofhir-server/src/test/scala/io/tofhir/server/endpoint/SchemaEndpointTest.scala
@@ -4,7 +4,7 @@ import akka.actor.ActorSystem
 import akka.http.scaladsl.model.{ContentType, ContentTypes, HttpEntity, MediaTypes, Multipart, StatusCodes}
 import akka.http.scaladsl.testkit.RouteTestTimeout
 import io.onfhir.api.client.FhirBatchTransactionRequestBuilder
-import io.onfhir.api.{FHIR_FOUNDATION_RESOURCES, Resource}
+import io.onfhir.api.{FHIR_DATA_TYPES, FHIR_FOUNDATION_RESOURCES, Resource}
 import io.tofhir.OnFhirTestContainer
 import io.tofhir.common.model.{DataTypeWithProfiles, SchemaDefinition, SimpleStructureDefinition}
 import io.tofhir.engine.model._
@@ -51,12 +51,12 @@ class SchemaEndpointTest extends BaseEndpointTest with OnFhirTestContainer {
   val schema3: SchemaDefinition = SchemaDefinition(url = "https://example.com/fhir/StructureDefinition/schema3", version = SchemaDefinition.VERSION_LATEST, `type` = "Ty3", name = "name3", description = Some("description3"), rootDefinition = None, fieldDefinitions = Some(
     Seq(
       SimpleStructureDefinition(id = "element-with-definition",
-        path = "Ty3.element-with-definition", dataTypes = Some(Seq(DataTypeWithProfiles(dataType = "canonical", profiles = Some(Seq("http://hl7.org/fhir/StructureDefinition/canonical"))))), isPrimitive = true,
+        path = "Ty3.element-with-definition", dataTypes = Some(Seq(DataTypeWithProfiles(dataType = FHIR_DATA_TYPES.CANONICAL, profiles = Some(Seq("http://hl7.org/fhir/StructureDefinition/canonical"))))), isPrimitive = true,
         isChoiceRoot = false, isArray = false, minCardinality = 0, maxCardinality = None,
         boundToValueSet = None, isValueSetBindingRequired = None, referencableProfiles = None, constraintDefinitions = None, sliceDefinition = None,
         sliceName = None, fixedValue = None, patternValue = None, referringTo = None, short = Some("element-with-definition"), definition = Some("element definition"), comment = None, elements = None),
       SimpleStructureDefinition(id = "element-with-no-definition",
-        path = "Ty3.element-with-no-definition", dataTypes = Some(Seq(DataTypeWithProfiles(dataType = "canonical", profiles = Some(Seq("http://hl7.org/fhir/StructureDefinition/canonical"))))), isPrimitive = true,
+        path = "Ty3.element-with-no-definition", dataTypes = Some(Seq(DataTypeWithProfiles(dataType = FHIR_DATA_TYPES.CANONICAL, profiles = Some(Seq("http://hl7.org/fhir/StructureDefinition/canonical"))))), isPrimitive = true,
         isChoiceRoot = false, isArray = false, minCardinality = 0, maxCardinality = None,
         boundToValueSet = None, isValueSetBindingRequired = None, referencableProfiles = None, constraintDefinitions = None, sliceDefinition = None,
         sliceName = None, fixedValue = None, patternValue = None, referringTo = None, short = Some("element-with-no-definition"), definition = None, comment = None, elements = None)
@@ -69,12 +69,12 @@ class SchemaEndpointTest extends BaseEndpointTest with OnFhirTestContainer {
   val schema4: SchemaDefinition = SchemaDefinition(url = "https://example.com/fhir/StructureDefinition/schema4", version = SchemaDefinition.VERSION_LATEST, `type` = "Ty4", name = "name4", description = Some("description4"), rootDefinition = None, fieldDefinitions = Some(
     Seq(
       SimpleStructureDefinition(id = "element-with-short",
-        path = "Ty4.element-with-short", dataTypes = Some(Seq(DataTypeWithProfiles(dataType = "canonical", profiles = Some(Seq("http://hl7.org/fhir/StructureDefinition/canonical"))))), isPrimitive = true,
+        path = "Ty4.element-with-short", dataTypes = Some(Seq(DataTypeWithProfiles(dataType = FHIR_DATA_TYPES.CANONICAL, profiles = Some(Seq("http://hl7.org/fhir/StructureDefinition/canonical"))))), isPrimitive = true,
         isChoiceRoot = false, isArray = false, minCardinality = 0, maxCardinality = None,
         boundToValueSet = None, isValueSetBindingRequired = None, referencableProfiles = None, constraintDefinitions = None, sliceDefinition = None,
         sliceName = None, fixedValue = None, patternValue = None, referringTo = None, short = Some("element-with-short"), definition = None, comment = None, elements = None),
       SimpleStructureDefinition(id = "element-with-no-short",
-        path = "Ty4.element-with-no-short", dataTypes = Some(Seq(DataTypeWithProfiles(dataType = "canonical", profiles = Some(Seq("http://hl7.org/fhir/StructureDefinition/canonical"))))), isPrimitive = true,
+        path = "Ty4.element-with-no-short", dataTypes = Some(Seq(DataTypeWithProfiles(dataType = FHIR_DATA_TYPES.CANONICAL, profiles = Some(Seq("http://hl7.org/fhir/StructureDefinition/canonical"))))), isPrimitive = true,
         isChoiceRoot = false, isArray = false, minCardinality = 0, maxCardinality = None,
         boundToValueSet = None, isValueSetBindingRequired = None, referencableProfiles = None, constraintDefinitions = None, sliceDefinition = None,
         sliceName = None, fixedValue = None, patternValue = None, referringTo = None, short = None, definition = None, comment = None, elements = None)
@@ -244,10 +244,10 @@ class SchemaEndpointTest extends BaseEndpointTest with OnFhirTestContainer {
         val schema: SchemaDefinition = JsonMethods.parse(responseAs[String]).extract[SchemaDefinition]
         val fieldDefinitions = schema.fieldDefinitions.get
         fieldDefinitions.size shouldEqual 4
-        fieldDefinitions.head.dataTypes.get.head.dataType shouldEqual "integer"
-        fieldDefinitions(1).dataTypes.get.head.dataType shouldEqual "date"
-        fieldDefinitions(2).dataTypes.get.head.dataType shouldEqual "dateTime"
-        fieldDefinitions(3).dataTypes.get.head.dataType shouldEqual "string"
+        fieldDefinitions.head.dataTypes.get.head.dataType shouldEqual FHIR_DATA_TYPES.INTEGER
+        fieldDefinitions(1).dataTypes.get.head.dataType shouldEqual FHIR_DATA_TYPES.DATE
+        fieldDefinitions(2).dataTypes.get.head.dataType shouldEqual FHIR_DATA_TYPES.DATETIME
+        fieldDefinitions(3).dataTypes.get.head.dataType shouldEqual FHIR_DATA_TYPES.STRING
       }
     }
 
