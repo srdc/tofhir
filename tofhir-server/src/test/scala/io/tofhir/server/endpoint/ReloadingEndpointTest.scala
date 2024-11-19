66728d61d2637d8e13839bfec1cc48a8900892db
okanmercan <okanmercan16@gmail.com>
2024-11-19 14:19:10 +0300

    :recycle: test: Update reloading endpoint test.

--- tofhir-server/src/test/scala/io/tofhir/server/endpoint/ReloadingEndpointTest.scala
+++ tofhir-server/src/test/scala/io/tofhir/server/endpoint/ReloadingEndpointTest.scala
@@ -1,45 +1,37 @@
 package io.tofhir.server.endpoint
 
 import akka.http.scaladsl.model.StatusCodes
+import io.onfhir.definitions.common.model.Json4sSupport.formats
 import io.tofhir.engine.util.FileUtils
 import io.tofhir.server.BaseEndpointTest
 import io.tofhir.server.model.Project
-import io.onfhir.definitions.common.model.Json4sSupport.formats
 import io.tofhir.server.repository.project.ProjectFolderRepository
 import io.tofhir.server.util.FileOperations
-import org.json4s.{JArray, JObject, JString}
-import org.json4s.jackson.{JsonMethods, Serialization}
+import org.json4s.jackson.JsonMethods
+import org.json4s.{JArray, JObject}
 
-import java.io.FileWriter
 
 class ReloadingEndpointTest extends BaseEndpointTest {
 
   "The service" should {
 
     "should reload projects successfully after updating project file" in {
-      // Read projects and update description fields from projects.json
+      // Read number of projects
       val projectsFile = FileUtils.getPath(ProjectFolderRepository.PROJECTS_JSON).toFile
       val parsedProjects = FileOperations.readFileIntoJson(projectsFile).asInstanceOf[JArray].arr.map(p => p.asInstanceOf[JObject])
-      val reloadedProjects = parsedProjects.map(project => {
-        project.mapField {
-          case ("description", _) => ("description", JString("reloaded"))
-          case otherwise => otherwise
-        }
-      })
-      val fw = new FileWriter(projectsFile)
-      try fw.write(Serialization.writePretty(reloadedProjects)) finally fw.close()
+      val numberOfprojects = parsedProjects.length
+      // Delete projects.json
+      projectsFile.delete()
 
       // Trigger reload endpoint
       Get(s"/${webServerConfig.baseUri}/${ReloadEndpoint.SEGMENT_RELOAD}") ~> route ~> check {
         status shouldEqual StatusCodes.NoContent
 
-        // Check project endpoint for whether project descriptions are updated
+        // Check project endpoint for whether number of created projects is correct
         Get(s"/${webServerConfig.baseUri}/${ProjectEndpoint.SEGMENT_PROJECTS}") ~> route ~> check {
           status shouldEqual StatusCodes.OK
           val projects: Seq[Project] = JsonMethods.parse(responseAs[String]).extract[Seq[Project]]
-          projects.foreach(project => {
-            project.description.get shouldEqual "reloaded"
-          })
+          projects.length shouldEqual numberOfprojects
         }
       }
     }
