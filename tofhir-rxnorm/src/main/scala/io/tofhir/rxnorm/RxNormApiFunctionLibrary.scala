b522181b8cc570c26e3cb48321478c44e8975932
emrecam <cam.emre090@gmail.com>
2025-01-17 15:45:25 +0300

    :recycle: Refactor(FhirPathFunctions): Update Fhir Path Functions based on the new documenatation model of Fhir Path Function class

--- tofhir-rxnorm/src/main/scala/io/tofhir/rxnorm/RxNormApiFunctionLibrary.scala
+++ tofhir-rxnorm/src/main/scala/io/tofhir/rxnorm/RxNormApiFunctionLibrary.scala
@@ -2,7 +2,7 @@ package io.tofhir.rxnorm
 
 import io.onfhir.api.FHIR_DATA_TYPES
 import io.onfhir.api.util.FHIRUtil
-import io.onfhir.path.annotation.FhirPathFunction
+import io.onfhir.path.annotation.{FhirPathFunction, FhirPathFunctionDocumentation, FhirPathFunctionParameter, FhirPathFunctionReturn}
 import io.onfhir.path.grammar.FhirPathExprParser.ExpressionContext
 import io.onfhir.path.{AbstractFhirPathFunctionLibrary, FhirPathComplex, FhirPathEnvironment, FhirPathException, FhirPathExpressionEvaluator, FhirPathNumber, FhirPathResult, FhirPathString, IFhirPathFunctionLibraryFactory}
 import org.json4s.JsonAST.{JArray, JDouble, JObject, JString}
@@ -22,15 +22,34 @@ class RxNormApiFunctionLibrary(rxNormApiClient:RxNormApiClient, context: FhirPat
    * @param ndcExpr
    * @return
    */
-   @FhirPathFunction(
-     documentation = "\uD83D\uDCDC Finds out RxNorm Concept Ids (rxcui) for the given NDC code.\n\n\uD83D\uDCDD <span style=\"color:#ff0000;\">_@param_</span> **`ndcExpr`**  \nThe NDC code expression.\n\n\uD83D\uDD19 <span style=\"color:#ff0000;\">_@return_</span>  \n```\n[\"104922\", \"105784\"]\n``` \n\uD83D\uDCA1 **E.g.** rxn:findRxConceptIdsByNdc('12345678901')",
-     insertText = "rxn:findRxConceptIdsByNdc(<ndcCode>)",
-     detail = "rxn",
-     label = "rxn:findRxConceptIdByNdc",
-     kind = "Function",
-     returnType = Seq(FHIR_DATA_TYPES.STRING),
-     inputType = Seq(FHIR_DATA_TYPES.STRING)
-   )
+  @FhirPathFunction(
+    documentation = FhirPathFunctionDocumentation(
+      detail = "Finds out RxNorm Concept Ids (rxcui) for the given NDC code.",
+      usageWarnings = None,
+      parameters = Some(Seq(
+        FhirPathFunctionParameter(
+          name = "ndcExpr",
+          detail = "The NDC code expression.",
+          examples = None
+        )
+      )),
+      returnValue = FhirPathFunctionReturn(
+        detail = None,
+        examples = Seq(
+          """["104922", "105784"]"""
+        )
+      ),
+      examples = Seq(
+        "rxn:findRxConceptIdsByNdc('12345678901')"
+      )
+    ),
+    insertText = "rxn:findRxConceptIdsByNdc(<ndcCode>)",
+    detail = "rxn",
+    label = "rxn:findRxConceptIdByNdc",
+    kind = "Function",
+    returnType = Seq(FHIR_DATA_TYPES.STRING),
+    inputType = Seq(FHIR_DATA_TYPES.STRING)
+  )
   def findRxConceptIdsByNdc(ndcExpr:ExpressionContext):Seq[FhirPathResult] = {
      val ndc: Option[String] =
        evaluator.visit(ndcExpr) match {
@@ -54,7 +73,26 @@ class RxNormApiFunctionLibrary(rxNormApiClient:RxNormApiClient, context: FhirPat
    * @return
    */
   @FhirPathFunction(
-    documentation = "\uD83D\uDCDC Finds out Ingredients and their properties for the given RxNorm Drug.\n\n\uD83D\uDCDD <span style=\"color:#ff0000;\">_@param_</span> **`rxcuiExpr`**  \nThe RxNorm Concept Id expression for the drug.\n\n\uD83D\uDD19 <span style=\"color:#ff0000;\">_@return_</span>  \n```json\n{\n  \"ingredientAndStrength\": [\n    {\n      \"activeIngredientRxcui\": \"104922\",\n      \"activeIngredientName\": \"Acetaminophen\",\n      \"numeratorValue\": 500.0,\n      \"numeratorUnit\": \"mg\",\n      \"denominatorValue\": 1.0,\n      \"denominatorUnit\": \"tablet\"\n    },\n    {\n      \"activeIngredientRxcui\": \"105784\",\n      \"activeIngredientName\": \"Ibuprofen\",\n      \"numeratorValue\": 200.0,\n      \"numeratorUnit\": \"mg\",\n      \"denominatorValue\": 1.0,\n      \"denominatorUnit\": \"tablet\"\n    }\n  ],\n  \"doseFormConcept\": {\n    \"rxcui\": \"106366\",\n    \"doseFormName\": \"Tablet\"\n  }\n}\n``` \n\uD83D\uDCA1 **E.g.** rxn:getMedicationDetails('12345')",
+    documentation = FhirPathFunctionDocumentation(
+      detail = "Finds out Ingredients and their properties for the given RxNorm Drug.",
+      usageWarnings = None,
+      parameters = Some(Seq(
+        FhirPathFunctionParameter(
+          name = "rxcuiExpr",
+          detail = "The RxNorm Concept Id expression for the drug.",
+          examples = None
+        )
+      )),
+      returnValue = FhirPathFunctionReturn(
+        detail = None,
+        examples = Seq(
+          """<JSON>{"ingredientAndStrength": [{"activeIngredientRxcui": "104922","activeIngredientName": "Acetaminophen","numeratorValue": 500.0,"numeratorUnit": "mg","denominatorValue": 1.0,"denominatorUnit": "tablet"},{"activeIngredientRxcui": "105784","activeIngredientName": "Ibuprofen","numeratorValue": 200.0,"numeratorUnit": "mg","denominatorValue": 1.0,"denominatorUnit": "tablet"}],"doseFormConcept": {"rxcui": "106366","doseFormName": "Tablet"}}"""
+        )
+      ),
+      examples = Seq(
+        "rxn:getMedicationDetails('12345')"
+      )
+    ),
     insertText = "rxn:getMedicationDetails(<rxcui>)",
     detail = "rxn",
     label = "rxn:getMedicationDetails",
@@ -131,7 +169,26 @@ class RxNormApiFunctionLibrary(rxNormApiClient:RxNormApiClient, context: FhirPat
    * @return
    */
   @FhirPathFunction(
-    documentation = "\uD83D\uDCDC Finds out Ingredients and their properties for the given RxNorm Drug.\n\n\uD83D\uDCDD <span style=\"color:#ff0000;\">_@param_</span> **`rxcuiExpr`**  \nThe RxNorm Concept Id expression for the drug.\n\n\uD83D\uDD19 <span style=\"color:#ff0000;\">_@return_</span>  \n```json\n[\n  {\n    \"ingredient\": \"Acetaminophen\",\n    \"strength\": \"500 mg\"\n  },\n  {\n    \"ingredient\": \"Ibuprofen\",\n    \"strength\": \"200 mg\"\n  }\n]\n``` \n\uD83D\uDCA1 **E.g.** rxn:findIngredientsOfDrug('12345')",
+    documentation = FhirPathFunctionDocumentation(
+      detail = "Finds out Ingredients and their properties for the given RxNorm Drug.",
+      usageWarnings = None,
+      parameters = Some(Seq(
+        FhirPathFunctionParameter(
+          name = "rxcuiExpr",
+          detail = "The RxNorm Concept Id expression for the drug.",
+          examples = None
+        )
+      )),
+      returnValue = FhirPathFunctionReturn(
+        detail = None,
+        examples = Seq(
+          """<JSON>[{"ingredient": "Acetaminophen","strength": "500 mg"}, {"ingredient": "Ibuprofen","strength": "200 mg"}]"""
+        )
+      ),
+      examples = Seq(
+        "rxn:findIngredientsOfDrug('12345')"
+      )
+    ),
     insertText = "rxn:findIngredientsOfDrug(<rxcui>)",
     detail = "rxn",
     label = "rxn:findIngredientsOfDrug",
@@ -158,7 +215,26 @@ class RxNormApiFunctionLibrary(rxNormApiClient:RxNormApiClient, context: FhirPat
    * @return
    */
   @FhirPathFunction(
-    documentation = "\uD83D\uDCDC Finds the corresponding ATC code for the given RxNorm ingredient concept.\n\n\uD83D\uDCDD <span style=\"color:#ff0000;\">_@param_</span> **`rxcuiExpr`**  \nThe RxNorm Concept Id expression for the drug.\n\n\uD83D\uDD19 <span style=\"color:#ff0000;\">_@return_</span>  \n```\n\"N02BE01\"\n``` \n\uD83D\uDCA1 **E.g.** rxn:getATC('12345')",
+    documentation = FhirPathFunctionDocumentation(
+      detail = "Finds the corresponding ATC code for the given RxNorm ingredient concept.",
+      usageWarnings = None,
+      parameters = Some(Seq(
+        FhirPathFunctionParameter(
+          name = "rxcuiExpr",
+          detail = "The RxNorm Concept Id expression for the drug.",
+          examples = None
+        )
+      )),
+      returnValue = FhirPathFunctionReturn(
+        detail = None,
+        examples = Seq(
+          "\"N02BE01\""
+        )
+      ),
+      examples = Seq(
+        "rxn:getATC('12345')"
+      )
+    ),
     insertText = "rxn:getATC(<rxcui>)",
     detail = "rxn",
     label = "rxn:getATC",
